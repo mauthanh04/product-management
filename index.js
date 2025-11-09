5cc3db54330a5c6241c410654b1bfe0df83280f0
mauthanh04 <mauthanh00004@gmail.com>
2025-11-10 00:09:54 +0700

    Kết nối mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mogoose = require('mongoose');
 require('dotenv').config();
 
 const routes = require('./routes/client/index.route');
+
+mogoose.connect(process.env.MONGO_URL)
 
 const app = express();
 const port = process.env.PORT;

85f94e06b742a50738af4377f0c95505c15c3abd
mauthanh04 <mauthanh00004@gmail.com>
2025-11-10 22:37:07 +0700

    lay ra data trang product admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const product = require("../../models/product.model");
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+    const products = await product.find({
+        deleted: false
+    });
     res.render("admin/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: products
     });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require('../../models/product.model');
 
+// [GET] /client/products
 module.exports.index =  async (req, res) => {
     const products = await Product.find({
         status: 'active',

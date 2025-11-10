617b3a33ffba68281fa34b601e28446607a61d63
mauthanh04 <mauthanh00004@gmail.com>
2025-11-10 11:18:34 +0700

    layout chung

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 require('dotenv').config();
 
 const database = require('./config/database');
-database.connect();
+
+const systemConfig = require('./config/system');
 
 const routes = require('./routes/client/index.route');
 const routesAdmin = require('./routes/admin/index.route');
+
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
 
 app.set('views', './views');
 app.set('view engine', 'pug') ;
+
+//app locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 app.use(express.static('public'));
 

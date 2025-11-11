d25b7a1b0f4ccc1cd4ed6d966b5435285b94fd23
mauthanh04 <mauthanh00004@gmail.com>
2025-11-11 14:23:03 +0700

    toi uu bo loc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,37 +1,13 @@
 const product = require("../../models/product.model");
+const filterStatusHelper = require("../../helpers/filterStatus.js");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: ""
-        }
-    
-    ];
+    const filterStatus = filterStatusHelper(req.query);
 
-    if (req.query.status) {
-        const index = filterStatus.findIndex(item => item.status === req.query.status);
-        filterStatus[index].class = "active";
-    }else {
-        const index = filterStatus.findIndex(item => item.status === "");
-        filterStatus[index].class = "active";
-    }
-
-   let find = {
-        deleted: false
-    };
+    let find = {
+            deleted: false
+        };
 
     if (req.query.status) { 
         find.status = req.query.status;
